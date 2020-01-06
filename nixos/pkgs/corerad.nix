2c90e95e5afb69235c6925118c4c8ba9a258b53d
Matt Layher <mdlayher@gmail.com>
2020-01-06 10:38:16 -0500

    nixos: bump CoreRAD v0.1.4

--- nixos/pkgs/corerad.nix
+++ nixos/pkgs/corerad.nix
@@ -2,7 +2,7 @@
 
 buildGoModule rec {
   pname = "corerad";
-  version = "0.1.3";
+  version = "0.1.4";
 
   goPackagePath = "github.com/mdlayher/corerad";
 
@@ -10,10 +10,10 @@ buildGoModule rec {
     owner = "mdlayher";
     repo = "corerad";
     rev = "v${version}";
-    sha256 = "0s17ffw4kx88gm165xjq1f415arcfpbjnbjppz0j2inz4n9kc25f";
+    sha256 = "0qlmmgdz69gqqn6h5kb3gsjyj7lm6pcfcx9xlmrxhisj914ij76r";
   };
 
-  modSha256 = "1lva0k8dn50grnx4daqk1pxgi6hn4yf5mhs37qi73gqp759c4q1l";
+  modSha256 = "0ln965zmpp9brdpadqi23yg5054kd4qf89hq2fppyx1gcididjyx";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/mdlayher/corerad";
