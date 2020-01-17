fa896789e688539fc775189e911a40b37b9ec416
Matt Layher <mdlayher@gmail.com>
2020-01-17 15:30:34 -0500

    nixos: enable automatic upgrades

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -132,9 +132,14 @@ in {
     ];
   };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system = {
+    # Automatic upgrades.
+    autoUpgrade = { enable = true; };
+
+    # This value determines the NixOS release with which your system is to be
+    # compatible, in order to avoid breaking some software such as database
+    # servers. You should change this only after NixOS release notes say you
+    # should.
+    stateVersion = "19.09"; # Did you read the comment?
+  };
 }
