b98c7c05d3903d729b536c01d66dc9f9228538fb
Matt Layher <mdlayher@gmail.com>
2020-02-12 12:00:16 -0500

    nixos: simplify CoreDNS config, add IPv4/6 subdomains

--- nixos/coredns.nix
+++ nixos/coredns.nix
@@ -21,53 +21,39 @@ in {
   services.coredns = {
     enable = true;
     config = ''
-      # DNS over TLS forwarding.
-      (dns_forward) {
+      # Root zone.
+      . {
+        cache 3600 {
+          success 8192
+          denial 4096
+        }
+        prometheus :9153
         forward . tls://8.8.8.8 tls://8.8.4.4 tls://2001:4860:4860::8888 tls://2001:4860:4860::8844 {
           tls_servername dns.google
           health_check 5s
         }
       }
 
-      # Trusted DNS.
-      . {
-        bind ${vars.localhost.ipv4} ${vars.localhost.ipv6} ${lab0.ipv4} ${lab0.ipv6.ula} ${lan0.ipv4} ${lan0.ipv6.ula} ${wg0.ipv4} ${wg0.ipv6.ula}
-        cache 3600 {
-          success 8192
-          denial 4096
-        }
-        prometheus :9153
-        import dns_forward
-      }
-
-      # Untrusted DNS.
-      . {
-        bind ${guest0.ipv4} ${guest0.ipv6.ula} ${iot0.ipv4} ${iot0.ipv6.ula}
-        log
-        import dns_forward
-      }
-
-      # Internal DNS.
+      # Internal zone.
       ${domain} {
-        bind ${vars.localhost.ipv4} ${vars.localhost.ipv6} ${lan0.ipv4} ${lan0.ipv6.ula} ${wg0.ipv4} ${wg0.ipv6.ula}
-        cache 3600 {
-          success 8192
-          denial 4096
-        }
-        hosts ${vars.domain} {
-          ${lan0.ipv4}     routnerr-2.${domain}
-          ${lan0.ipv6.ula} routnerr-2.${domain}
-
+        hosts {
           ${
           # Write out internal DNS records for each of the configured hosts.
           # If the host does not have an IPv6 ULA address, omit it.
             lib.concatMapStrings (host: ''
               ${host.ipv4} ${host.name}.${domain}
+              ${host.ipv4} ${host.name}.ipv4.${domain}
+
               ${if host.ipv6.ula != "" then ''
                 ${host.ipv6.ula} ${host.name}.${domain}
+                ${host.ipv6.ula} ${host.name}.ipv6.${domain}
               '' else
                 ""}
-            '') (vars.hosts.servers ++ vars.hosts.infra)
+            '') (vars.hosts.servers ++ vars.hosts.infra ++ [{
+              name = "routnerr-2";
+              ipv4 = lan0.ipv4;
+              ipv6.ula = lan0.ipv6.ula;
+            }])
           }
         }
       }
