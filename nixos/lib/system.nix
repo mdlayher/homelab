34a4a910a9d51c5c39e503bfee210f20fcce09de
Matt Layher <mdlayher@gmail.com>
2020-08-18 12:59:49 -0400

    nixos/lib: install comma on all machines
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- nixos/lib/system.nix
+++ nixos/lib/system.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
-let secrets = import ./secrets.nix;
+let
+  secrets = import ./secrets.nix;
+
+  # Import comma with local nix-index preferred over the comma one.
+  comma = import (builtins.fetchTarball "https://github.com/Shopify/comma/archive/60a4cf8ec5c93104d3cfb9fc5a5bac8fb18cc8e4.tar.gz") { inherit pkgs; };
 
 in {
   # Console configuration.
@@ -22,6 +26,7 @@ in {
       bandwhich
       bc
       byobu
+      comma
       dmidecode
       ethtool
       gcc
