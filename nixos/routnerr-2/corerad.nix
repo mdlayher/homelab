e4ce57cc5425ddeec638ac6adf7cbac89be200cb
Matt Layher <mdlayher@gmail.com>
2021-01-03 18:00:41 -0500

    nixos/routnerr-2: enable verbose CoreRAD logs
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- nixos/routnerr-2/corerad.nix
+++ nixos/routnerr-2/corerad.nix
@@ -35,6 +35,9 @@ in {
           # Configure a higher preference for interfaces with more bandwidth.
           preference = ifi.preference;
 
+          # Enable verbose logs so we can identify client solicitation patterns.
+          verbose = true;
+
           # Advertise all /64 prefixes on the interface.
           prefix = [{ prefix = "::/64"; }];
 
