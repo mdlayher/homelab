88f43573dc8ded7c5fff8c4aed89b59e37c08354
Matt Layher <mdlayher@gmail.com>
2021-02-16 14:06:17 -0500

    nixos/servnerr-3: add workaround for NixOS container shutdown
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- nixos/servnerr-3/containers.nix
+++ nixos/servnerr-3/containers.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # These services are proprietary and run containerized for confinement from
@@ -49,6 +49,32 @@
         ports = [ "9091:8080" ];
         volumes = [ "/var/lib/promlens:/var/lib/promlens" ];
       };
+    };
+  };
+
+  # Workaround for NixOS containers not stopping at reboot, see:
+  # https://github.com/NixOS/nixpkgs/issues/109695#issuecomment-774662261
+  systemd.services."shutdown-containers" = {
+    description = "Workaround for nixos-containers shutdown";
+    enable = true;
+
+    unitConfig = {
+      DefaultDependencies = false;
+      RequiresMountFor = "/";
+    };
+
+    before = [ "shutdown.target" "reboot.target" "halt.target" "final.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = pkgs.writeScript "containers-shutdown" ''
+        #! ${pkgs.runtimeShell} -e
+        ${pkgs.nixos-container}/bin/nixos-container list | while read container; do
+          ${pkgs.nixos-container}/bin/nixos-container stop $container
+        done
+      '';
     };
   };
 }
