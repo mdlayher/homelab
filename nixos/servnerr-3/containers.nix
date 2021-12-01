36b635d4091d59e59a9d62afc119dc33e8ec2fee
Matt Layher <mdlayher@gmail.com>
2021-12-01 13:59:50 -0500

    nixos/servnerr-3: remove minecraft
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- nixos/servnerr-3/containers.nix
+++ nixos/servnerr-3/containers.nix
@@ -4,55 +4,6 @@
   # These services are proprietary and run containerized for confinement from
   # the rest of the system and on unstable for faster update cycles.
   containers = {
-    minecraft = {
-      autoStart = true;
-      bindMounts = {
-        # Mount the data directory as read/write.
-        "/var/lib/minecraft" = {
-          hostPath = "/var/lib/minecraft";
-          isReadOnly = false;
-        };
-      };
-      config = { ... }:
-        let
-          secrets = import ./lib/secrets.nix;
-          unstable =
-            import <nixos-unstable-small> { config.allowUnfree = true; };
-        in {
-          services.minecraft-server = {
-            enable = true;
-            package = unstable.minecraft-server;
-
-            eula = true;
-            declarative = true;
-
-            # Use more RAM!
-            jvmOpts = "-Xmx16384M -Xms16384M";
-
-            serverProperties = {
-              motd = "Matt's Minecraft server";
-              enable-rcon = true;
-              "rcon.password" = secrets.minecraft.rcon;
-              pvp = false;
-              view-distance = 15;
-              enforce-whitelist = true;
-              white-list = true;
-            };
-
-            whitelist = {
-              blalex93 = "b0d98aa2-8284-4830-b06e-a205ee0f166b";
-              DocNastyDub = "d849719b-2438-4ecc-8557-7decc63ad5cb";
-              ericthegreat12 = "6853f664-f600-42c5-9d82-934d0ab6df9c";
-              HashtagVEGAS = "2dfdd3a5-876b-4042-b2bf-5a49621e998f";
-              nerrster = "38f8c307-dde4-4774-a969-f4cc69dec50e";
-              rothberry = "5d758298-b38b-4e30-8e02-161373b26c01";
-              Son_of_a_Teacher = "42cc4da3-3d2a-4cc3-9b2d-897f93438594";
-              TheWrat = "dcc01480-e7d9-4f1d-9eb2-87ebb1e6af74";
-            };
-          };
-        };
-    };
-
     plex = {
       autoStart = true;
       bindMounts = {
