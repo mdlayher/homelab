fcf41cd30c48b53018e2083ce5e41f559363fc51
Matt Layher <mdlayher@gmail.com>
2022-03-03 11:33:03 -0500

    nixos/routnerr-2: further simplify networkd config
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- nixos/routnerr-2/networking.nix
+++ nixos/routnerr-2/networking.nix
@@ -21,6 +21,35 @@ let
       vlanConfig.Id = id;
     }));
 
+  vlanNetwork = (name: (id: {
+      matchConfig.Name = name;
+      # Embed ID directly in IPv4/6 addresses for clarity.
+      address = [ "fd9e:1a04:f01d:${toString id}::1/64" "192.168.${toString id}.1/24" ];
+      networkConfig.DHCPv6PrefixDelegation = true;
+      dhcpV6PrefixDelegationConfig = {
+        # Router always lives at ::1.
+        Token = "::1";
+        # Delegate the associated hex subnet ID from DHCPv6-PD.
+        SubnetId = "${toString (decToHex id)}";
+      };
+    }));
+
+  # Thanks, corpix!
+  # https://gist.github.com/corpix/f761c82c9d6fdbc1b3846b37e1020e11
+  decToHex =
+    let
+      intToHex = [
+        "0" "1" "2" "3" "4" "5" "6" "7" "8" "9"
+        "a" "b" "c" "d" "e" "f"
+      ];
+      toHex' = q: a:
+        if q > 0
+        then (toHex'
+          (q / 16)
+          ((lib.elemAt intToHex (lib.mod q 16)) + a))
+        else a;
+    in
+      v: toHex' v "";
 in {
   networking = {
     hostName = "routnerr-2";
@@ -78,36 +107,10 @@ in {
       };
     };
 
-    # Wireless WAN.
+    # Wireless WAN, temporarily unused.
     links."11-wwan0" = {
       matchConfig.Path = "pci-0000:00:13.0-usb-0:1.3:1.12";
       linkConfig.Name = "wwan0";
-    };
-    networks."11-wwan0" = {
-      # Disabled; waiting on networkd + ModemManager integration.
-      enable = false;
-
-      matchConfig.Name = "wwan0";
-      networkConfig = {
-        DHCP = "yes";
-        DefaultRouteOnDevice = false;
-      };
-      # Do not require WWAN for online.
-      linkConfig.RequiredForOnline = false;
-      # Never accept ISP DNS or search domains.
-      dhcpV4Config = {
-        UseDNS = false;
-        UseDomains = false;
-      };
-      dhcpV6Config = {
-        UseDNS = false;
-        # TODO(mdlayher): NixOS doesn't allow this?
-        # UseDomains = false;
-      };
-      ipv6AcceptRAConfig = {
-        UseDNS = false;
-        UseDomains = false;
-      };
     };
 
     # Physical management LAN. For physical LANs, we have to make sure to match
@@ -115,6 +118,9 @@ in {
     links."15-mgmt0" = ethLink "mgmt0" "00:0d:b9:53:ea:cd";
     networks."15-mgmt0" = {
       matchConfig.Name = "mgmt0";
+
+      # TODO(mdlayher): eventually it'd be nice to renumber this as
+      # 192.168.0.1/24 but that would require a lot of device churn.
       address = [ "fd9e:1a04:f01d::1/64" "192.168.1.1/24" ];
 
       # VLANs associated with this physical interface.
@@ -133,51 +139,19 @@ in {
 
     # Home VLAN.
     netdevs."20-lan0" = vlanNetdev "lan0" 10;
-    networks."20-lan0" = {
-      matchConfig.Name = "lan0";
-      address = [ "fd9e:1a04:f01d:10::1/64" "192.168.10.1/24" ];
-      networkConfig.DHCPv6PrefixDelegation = true;
-      dhcpV6PrefixDelegationConfig = {
-        Token = "::1";
-        SubnetId = "a";
-      };
-    };
+    networks."20-lan0" = vlanNetwork "lan0" 10;
 
     # IoT VLAN.
     netdevs."25-iot0" = vlanNetdev "iot0" 66;
-    networks."25-iot0" = {
-      matchConfig.Name = "iot0";
-      address = [ "fd9e:1a04:f01d:66::1/64" "192.168.66.1/24" ];
-      networkConfig.DHCPv6PrefixDelegation = true;
-      dhcpV6PrefixDelegationConfig = {
-        Token = "::1";
-        SubnetId = "42";
-      };
-    };
+    networks."25-iot0" = vlanNetwork "iot0" 66;
 
     # Guest VLAN.
     netdevs."30-guest0" = vlanNetdev "guest0" 9;
-    networks."30-guest0" = {
-      matchConfig.Name = "guest0";
-      address = [ "fd9e:1a04:f01d:9::1/64" "192.168.9.1/24" ];
-      networkConfig.DHCPv6PrefixDelegation = true;
-      dhcpV6PrefixDelegationConfig = {
-        Token = "::1";
-        SubnetId = "9";
-      };
-    };
+    networks."30-guest0" = vlanNetwork "guest0" 9;
 
     # Lab VLAN.
     netdevs."35-lab0" = vlanNetdev "lab0" 2;
-    networks."35-lab0" = {
-      matchConfig.Name = "lab0";
-      address = [ "fd9e:1a04:f01d:2::1/64" "192.168.2.1/24" ];
-      networkConfig.DHCPv6PrefixDelegation = true;
-      dhcpV6PrefixDelegationConfig = {
-        Token = "::1";
-        SubnetId = "2";
-      };
-    };
+    networks."35-lab0" = vlanNetwork "lab0" 2;
 
     # WireGuard tunnel.
     netdevs."40-wg0" = {
