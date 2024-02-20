c84446a6c4d7ba85c883d806497b23c51d790a1f
Matt Layher <mdlayher@gmail.com>
2024-02-20 11:00:03 -0500

    nixos/lib: add mdlayher account

--- nixos/lib/system.nix
+++ nixos/lib/system.nix
@@ -128,9 +128,23 @@ in {
     mutableUsers = false;
 
     # Set up matt's account, enable sudo and SSH login.
+    #
+    # TODO: remove pending migration.
     users.matt = {
       isNormalUser = true;
       uid = 1000;
+      extraGroups = [ "dialout" "libvirtd" "networkmanager" "wheel" ];
+      hashedPassword = secrets.users.matt_password_hash;
+
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN5i5d0mRKAf02m+ju+I1KrAYw3Ny2IHXy88mgyragBN Matt Layher (mdlayher@gmail.com)"
+      ];
+    };
+
+    # Set up user account, enable sudo and SSH login.
+    users.mdlayher = {
+      isNormalUser = true;
+      uid = 1001;
       extraGroups = [ "dialout" "libvirtd" "networkmanager" "wheel" ];
       hashedPassword = secrets.users.matt_password_hash;
 
