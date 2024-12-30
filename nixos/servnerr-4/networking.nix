60f2766fa080bb413d911ba6c55b40a679fb7422
Matt Layher <mdlayher@gmail.com>
2024-12-30 13:33:09 -0500

    nixos/servnerr-4: fix tailscaled system ordering

--- nixos/servnerr-4/networking.nix
+++ nixos/servnerr-4/networking.nix
@@ -27,10 +27,13 @@ in
   # Tailscale readiness and DNS tweaks.
   systemd.network.wait-online.ignoredInterfaces = [ "ts0" ];
 
-  systemd.services.tailscaled.after = [
-    "network-online.target"
-    "systemd-resolved.service"
-  ];
+  systemd.services.tailscaled = {
+    after = [
+      "network-online.target"
+      "systemd-resolved.service"
+    ];
+    wants = [ "network-online.target" ];
+  };
 
   systemd.network = {
     enable = true;
