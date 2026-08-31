e162849aaf9ead92428bb1bb4e040211b2da1a50
Matt Layher <mdlayher@gmail.com>
2026-08-31 11:08:49 -0400

    nixos/routnerr-3: mdlayher
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- nixos/routnerr-3/configuration.nix
+++ nixos/routnerr-3/configuration.nix
@@ -18,6 +18,9 @@ in
 
   system.stateVersion = "23.05";
 
+  # Transitional: this machine uses the new admin username.
+  homelab.user = "mdlayher";
+
   # TODO: https://github.com/NixOS/nixos-hardware/pull/673
   boot.kernelParams = [ "console=ttyS0,115200n8" ];
 
