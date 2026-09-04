1bddf039c3581783a92586d587e6cf1f5870c7c8
Matt Layher <mdlayher@gmail.com>
2026-09-04 13:27:37 -0400

    terraform/cloudflare: final zones
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- terraform/cloudflare/locals.tf
+++ terraform/cloudflare/locals.tf
@@ -3,7 +3,7 @@ locals {
   zones = {
     "mdlayher.com" = "db4f8fadfe5bffe77456ad42c700e52a"
     "corerad.net"  = "4bd7e44515294fdb382d526cd6e35d14"
-    "servnerr.com" = "REPLACE_ME"
-    "mdlayher.net" = "REPLACE_ME"
+    "servnerr.com" = "657638d5af57c1e06f860ac5fed17024"
+    "mdlayher.net" = "7d358bd5ac55e8d46dcffb6afaf86854"
   }
 }
