87e2f966d6f3b077e9fef304d9487c319d2b6601
Matt Layher <mdlayher@gmail.com>
2026-09-04 13:12:19 -0400

    terraform/cloudflare: corerad.net
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- terraform/cloudflare/locals.tf
+++ terraform/cloudflare/locals.tf
@@ -2,7 +2,7 @@ locals {
   # From each zone's overview page in the dashboard. Not secrets.
   zones = {
     "mdlayher.com" = "db4f8fadfe5bffe77456ad42c700e52a"
-    "corerad.net"  = "REPLACE_ME"
+    "corerad.net"  = "4bd7e44515294fdb382d526cd6e35d14"
     "servnerr.com" = "REPLACE_ME"
     "mdlayher.net" = "REPLACE_ME"
   }
