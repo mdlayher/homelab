91a74117f8e32293574e5c37a2116a4c0f6704de
Matt Layher <mdlayher@gmail.com>
2026-09-04 12:26:23 -0400

    secrets: cloudflare init
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- terraform/cloudflare/locals.tf
+++ terraform/cloudflare/locals.tf
@@ -1,7 +1,7 @@
 locals {
   # From each zone's overview page in the dashboard. Not secrets.
   zones = {
-    "mdlayher.com" = "REPLACE_ME"
+    "mdlayher.com" = "db4f8fadfe5bffe77456ad42c700e52a"
     "corerad.net"  = "REPLACE_ME"
     "servnerr.com" = "REPLACE_ME"
     "mdlayher.net" = "REPLACE_ME"
